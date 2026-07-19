e85bf9b722720ec66c8499ed01a2dc94f477e19f
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-20 00:57:26 +0330

    Phase 15H: allow template creation

--- app/Filament/Resources/NotificationTemplateResource/Pages/ManageNotificationTemplates.php
+++ app/Filament/Resources/NotificationTemplateResource/Pages/ManageNotificationTemplates.php
@@ -3,9 +3,15 @@
 namespace App\Filament\Resources\NotificationTemplateResource\Pages;
 
 use App\Filament\Resources\NotificationTemplateResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
 
 class ManageNotificationTemplates extends ManageRecords
 {
     protected static string $resource = NotificationTemplateResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [Actions\CreateAction::make()];
+    }
 }
