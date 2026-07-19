42d6ed446e5d845eb9e5f951dbe2b077a8759c8d
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-19 16:39:14 +0330

    style: import bakery variant model for resource annotation

--- app/Http/Resources/BakeryProductResource.php
+++ app/Http/Resources/BakeryProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\BakeryProductVariant;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Collection;
@@ -10,7 +11,7 @@ class BakeryProductResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        /** @var Collection<int, \App\Models\BakeryProductVariant> $variants */
+        /** @var Collection<int, BakeryProductVariant> $variants */
         $variants = $this->resource->relationLoaded('activeVariants')
             ? $this->activeVariants
             : collect();
