980bbac7ea743515a2da4026f8276662e046be14
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-19 16:25:19 +0330

    feat: add bakery category create page

---
+++ app/Filament/Resources/BakeryCategoryResource/Pages/CreateBakeryCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\BakeryCategoryResource\Pages;
+
+use App\Filament\Resources\BakeryCategoryResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBakeryCategory extends CreateRecord
+{
+    protected static string $resource = BakeryCategoryResource::class;
+}
