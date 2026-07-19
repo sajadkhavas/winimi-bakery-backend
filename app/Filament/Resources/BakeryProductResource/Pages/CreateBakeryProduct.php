c79e7a19b06cc867379beef9d513cd0e10cfb0c7
sajadkhavas <144109918+sajadkhavas@users.noreply.github.com>
2026-07-19 16:27:45 +0330

    feat: add bakery product create page

---
+++ app/Filament/Resources/BakeryProductResource/Pages/CreateBakeryProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\BakeryProductResource\Pages;
+
+use App\Filament\Resources\BakeryProductResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBakeryProduct extends CreateRecord
+{
+    protected static string $resource = BakeryProductResource::class;
+}
